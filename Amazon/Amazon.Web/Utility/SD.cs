40918aab61ec1be63f06f183ec4ecee0549c4e33
mmraihan <mm_raihan@live.com>
2023-10-06 14:48:02 +0600

    Added MVC project with some folder.

---
+++ Amazon/Amazon.Web/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace Amazon.Web.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
