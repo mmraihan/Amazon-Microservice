b371b15c16664d75d091cfe89bcd4a89531189a5
mmraihan <mm_raihan@live.com>
2023-10-06 13:03:50 +0600

    Coupon API CUD cmpleted.

--- Amazon/Amazon.Services.CouponApi/Controllers/CouponAPIController.cs
+++ Amazon/Amazon.Services.CouponApi/Controllers/CouponAPIController.cs
@@ -65,5 +65,84 @@ namespace Amazon.Services.CouponApi.Controllers
             return _response;
         }
 
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(c => c.CouponCode.ToLower() == code.ToLower());            
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+
+        [HttpPost]
+        public ResponseDto Post([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Add(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDto Put([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<CouponDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon coupon= _db.Coupons.First(c=>c.CouponId==id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
     }
 }
--- Amazon/Amazon.Services.CouponApi/Model/Dto/CouponDto.cs
+++ Amazon/Amazon.Services.CouponApi/Model/Dto/CouponDto.cs
@@ -6,6 +6,6 @@
         public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
-        public int MaxAmount { get; set; } = 12;
+        
     }
 }
