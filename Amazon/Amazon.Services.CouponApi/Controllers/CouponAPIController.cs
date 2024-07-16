a9207014483357aaac0d87309322bd0253983caf
mmraihan <mm_raihan@live.com>
2024-07-16 17:52:45 +0600

    CouponService: Coupon controller with response dto.

---
+++ Amazon/Amazon.Services.CouponApi/Controllers/CouponAPIController.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.Services.CouponAPI.Data;
+using Amazon.Services.CouponAPI.Dtos;
+using Amazon.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.Services.CouponAPI.Controllers
+{
+
+    public class CouponAPIController : BaseAPIController
+    {
+
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponAPIController(AppDbContext db, ResponseDto response)
+        {
+            _db = db;
+            _response = response;
+        }
+
+        //[HttpGet]
+
+        //public ResponseDto Get()
+        //{
+        //    try
+        //    {
+        //        var couponList = _db.Coupons.ToList();
+        //        _response.Result = couponList;
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw;
+        //    }
+        //}
+
+       
+    }
+}
