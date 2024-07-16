98d32ac12d4ec572cec68b0736807918b27b0fad
mmraihan <mm_raihan@live.com>
2024-07-16 16:03:59 +0600

    CouponService: Configure Connection String, Coupon Entity and Necessary Libraries added.

---
+++ Amazon/Amazon.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amazon.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+    }
+}
--- Amazon/Amazon.Services.CouponApi/Program.cs
+++ Amazon/Amazon.Services.CouponApi/Program.cs
@@ -1,15 +1,25 @@
+using Amazon.Services.CouponAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+#region Additional Services
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+#endregion
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
