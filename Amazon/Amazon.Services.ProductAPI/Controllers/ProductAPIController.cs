05ad4f6adf6782f7e0be49f7a60e0e17af07fb54
mmraihan <mm_raihan@live.com>
2024-07-29 00:07:13 +0600

    Product: Product CRUD Implemeted with Authentication.

---
+++ Amazon/Amazon.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,129 @@
+﻿using Amazon.Services.ProductAPI.Data;
+using Amazon.Services.ProductAPI.Dtos;
+using Amazon.Services.ProductAPI.Models;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Amazon.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    [Authorize]
+    public class ProductAPIController : ControllerBase
+    {
+
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+        private IMapper _mapper;
+
+        public ProductAPIController(AppDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                var obj = _db.Products   ;
+                _response.Result = _mapper.Map<List<ProductDto>>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product obj = _db.Products.First(u => u.ProductId == id);
+                _response.Result = _mapper.Map<ProductDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+
+
+        [HttpPost]
+        [Authorize(Roles ="ADMIN")]
+        public ResponseDto Post([FromBody] ProductDto ProductDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(ProductDto);
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put([FromBody] ProductDto ProductDto)
+        {
+            try
+            {
+                Product obj = _mapper.Map<Product>(ProductDto);
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                _response.Result = _mapper.Map<ProductDto>(obj);
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product Product = _db.Products.First(c => c.ProductId == id);
+                _db.Products.Remove(Product);
+                _db.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+
+            }
+            return _response;
+        }
+
+
+    }
+}
---
+++ Amazon/Amazon.Services.ProductAPI/Migrations/20240728175955_ProductTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Amazon.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Electronics", "High performance laptop with 16GB RAM and 512GB SSD.<br/> Ideal for gaming, programming, and design.", "https://placehold.co/603x403", "Laptop", 1000.0 },
+                    { 2, "Electronics", "Latest smartphone with 5G connectivity and 128GB storage.<br/> Features a stunning display and excellent camera.", "https://placehold.co/602x402", "Smartphone", 699.99000000000001 },
+                    { 3, "Wearables", "Smartwatch with heart rate monitor and GPS tracking.<br/> Perfect for fitness enthusiasts and tech lovers.", "https://placehold.co/601x401", "Smartwatch", 199.99000000000001 },
+                    { 4, "Electronics", "Versatile tablet with 10.5-inch display and 64GB storage.<br/> Great for entertainment and productivity on the go.", "https://placehold.co/600x400", "Tablet", 329.99000000000001 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
