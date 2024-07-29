109a848a9c32bf5debad03c08142408821253be2
mmraihan <mm_raihan@live.com>
2024-07-29 22:34:28 +0600

    Product: Implemented all View in Product controller.

--- Amazon/Amazon.Web/Controllers/ProductController.cs
+++ Amazon/Amazon.Web/Controllers/ProductController.cs
@@ -30,7 +30,6 @@ namespace Amazon.Web.Controllers
             return View(list);
         }
 
-
         public async Task<IActionResult> ProductCreate()
         {
             return View();
@@ -85,6 +84,42 @@ namespace Amazon.Web.Controllers
             else
             {
                 TempData["error"] = response?.Message;
+            }
+            return View(productDto);
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductDto? model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductEdit(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _productService.UpdateProductsAsync(productDto);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product updated successfully";
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
             }
             return View(productDto);
         }
