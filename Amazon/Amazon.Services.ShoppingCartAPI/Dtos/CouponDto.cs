f5f710549a49575f1207c7820a2ecc7e4f8ac79f
mmraihan <mm_raihan@live.com>
2024-07-30 16:49:53 +0600

    Cart: Necessary Entities and Dtos added.

---
+++ Amazon/Amazon.Services.ShoppingCartAPI/Dtos/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Amazon.Services.ShoppingCartAPI.Dtos
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
