5ddab050940a0769e1eab4bba3452b9c2ec288ea
mmraihan <mm_raihan@live.com>
2024-08-18 13:55:37 +0600

    Cart: Apply Coupon , remove coupon end point added.

---
+++ Amazon/Amazon.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Services.ShoppingCartAPI.Dtos;
+using Amazon.Services.ShoppingCartAPI.Service.IService;
+
+namespace Amazon.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        public Task<CouponDto> GetCoupon(string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Amazon/Amazon.Services.ShoppingCartAPI/Service/IService/ICouponService.cs
@@ -0,0 +1,9 @@
+﻿using Amazon.Services.ShoppingCartAPI.Dtos;
+
+namespace Amazon.Services.ShoppingCartAPI.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetCoupon(string couponCode);
+    }
+}
